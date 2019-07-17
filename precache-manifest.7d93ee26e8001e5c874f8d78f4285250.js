b0a69a2e5ace321446b194c30ef9c26f240939e2
Marcelo Pecin <celopeccin@gmail.com>
2019-07-17 19:29:43 -0300

    Updates

---
+++ precache-manifest.7d93ee26e8001e5c874f8d78f4285250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c21b39474adda2c6b7bd",
+    "url": "/static/js/main.c0cd1369.chunk.js"
+  },
+  {
+    "revision": "eba73921d61b2f47f024",
+    "url": "/static/js/2.d7f4798b.chunk.js"
+  },
+  {
+    "revision": "c21b39474adda2c6b7bd",
+    "url": "/static/css/main.7ecfadfb.chunk.css"
+  },
+  {
+    "revision": "a04f5063d94484215f2233d5fcd1eff9",
+    "url": "/index.html"
+  }
+];
